893403123cc6de4ef847cbcd645690ca5179a7c0
createransw <164249698+createransw@users.noreply.github.com>
2025-11-11 23:40:14 +0300

    Autotest (#3)
    * template for project
    
    * fix doing nothing
    
    * script to set up the environment
    
    * Spelling
    
    * Pre-commit hooks: black, isort, flake8 etc.
    
    * fixed requirenments
    
    * Pytest example
    
    * automatic tests on push
    
    * test tests
    
    * renaming
    
    * badge
    
    * badge fix
    
    * coverage % badge
    
    * next try
    
    * new try
    
    * new try
    
    * Revert "Merge branch 'main' into autotest"
    
    This reverts commit 7696bc1aa975735eaab9687390611d4c91067351, reversing
    changes made to 096ffaf1800afbcbfa10427f0c4f827364d25b41.
    
    * reset branch
    
    * Reapply "Merge branch 'main' into autotest"
    
    This reverts commit 942d8b0e69f0458a7b144c0ef4396a8ac1ed9ab8.
    
    * Revert "Reapply "Merge branch 'main' into autotest""
    
    This reverts commit b3baa67ddad1150bef54006938a5abf8da63aea3.
    
    * Reapply "reset branch"
    
    This reverts commit 258ea1355e88d4fd6e9856b1320f24cae91bb265.
    
    * Reapply "Reapply "Merge branch 'main' into autotest""
    
    This reverts commit d019c77a25da4c3b1eab31d31966e32a743e1921.
    
    * Revert "Reapply "Merge branch 'main' into autotest""
    
    This reverts commit 1426df1312c193014b95c39a8fc0c24e45bea6e7.
    
    * Revert "reset branch"
    
    This reverts commit 9ce3ee3caf324c6c6f03d01f394a5260ca90c8ea.
    
    * Reapply "Merge branch 'main' into autotest"
    
    This reverts commit 942d8b0e69f0458a7b144c0ef4396a8ac1ed9ab8.
    
    * Revert "Merge branch 'main' into autotest"
    
    This reverts commit 7696bc1aa975735eaab9687390611d4c91067351, reversing
    changes made to 096ffaf1800afbcbfa10427f0c4f827364d25b41.
    
    ---------
    
    Co-authored-by: Maksim <Maksim>
    Co-authored-by: YGtch <yulik.gashenko@yandex.ru>

--- tests/example/test_example.py
+++ tests/example/test_example.py
@@ -1,1 +1,14 @@
-# to be done
+import pytest
+
+
+@pytest.mark.parametrize("a,b,c", [(1, 1, 2), (2, 3, 5)])
+def test_sum(a, b, c):
+    assert a + b == c
+
+
+@pytest.mark.parametrize(
+    "file,text", [("tests/example/data/text.txt", "Hello, world!")]
+)
+def test_file(file, text):
+    with open(file) as data:
+        assert data.read().strip() == text
